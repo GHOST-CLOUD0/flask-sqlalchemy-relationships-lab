f2b27425353ba5d7261b8774f1eb1954c151d44a
Mark Kinyanjui <mckinyanjui3@gmail.com>
2026-07-22 15:29:05 +0300

    corrections

--- server/app.py
+++ server/app.py
@@ -15,7 +15,6 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
 
 migrate = Migrate(app, db) if Migrate else None
 db.init_app(app)
